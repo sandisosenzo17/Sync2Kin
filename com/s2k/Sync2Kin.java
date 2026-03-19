2df9ea7b0f70be395d98371c44eeeba009392cae
sandisosenzo17 <sandisosenzo17@gmail.com>
2026-03-20 01:08:50 +0200

    Initial setup of the project and .gitignore file. Setup is subject to change with additional features.

---
+++ com/s2k/Sync2Kin.java
@@ -0,0 +1,24 @@
+package com.s2k;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SuppressWarnings("unused")
+public class Sync2Kin {
+	private String parent_folder; // Home directory of the app
+	private int folder_count; // Number of watched folders
+	private Map<String, String> folders; // List of all the monitored folders
+	
+	public Sync2Kin() {
+		
+	}
+	
+	
+	
+
+	private String createFolder(String f_name) {
+		
+		
+		return "";
+	}
+}
